b0986a6f21fe421232e7302ba9d4bfe9ccc0f068
MuhDavin213 <davin.new58@gmail.com>
2023-12-06 00:41:48 +0700

    Add data CLASS Homeviewmodel

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     companion object{
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+
+    )
 }
