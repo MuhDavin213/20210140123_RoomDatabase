2dcfa2dd690ea411bd590311614e02c2870264f3
MuhDavin213 <davin.new58@gmail.com>
2023-12-06 10:23:00 +0700

    Add function EntrySiswaScreen

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,2 +1,16 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.model.EntryViewModel
+import com.example.roomsiswa.model.PenyediaViewModel
+
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
