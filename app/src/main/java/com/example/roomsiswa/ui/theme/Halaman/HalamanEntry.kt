392e1cb17a411360d64d2ef68e7ad35fa4249f80
MuhDavin213 <davin.new58@gmail.com>
2023-12-06 18:56:49 +0700

    Add Function Entry Siswa Body ,add container dengan scaffol dan isinya siswa top app bar dan entry siswa body

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -14,9 +14,13 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.model.EntryViewModel
 import com.example.roomsiswa.model.PenyediaViewModel
+import com.example.roomsiswa.model.UIStateSiswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
+import com.example.roomsiswa.navigasi.SiswaTopAppBar
+import kotlinx.coroutines.launch
 
 object  DestinasiEntry: DestinasiNavigasi{
     override val route = "item_entry"
@@ -28,5 +32,39 @@ fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+){    val coroutineScope = rememberCoroutineScope()
+    val scroolBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scroolBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scroolBehavior
+            )
+        }) {innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch{
+                    viewModel.saveSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+
+        )
+    }
+}
+
+@Composable
+fun EntrySiswaBody(uiStateSiswa: UIStateSiswa,
+                   onSiswaValueChange: (DetailSiswa) -> Unit,
+                   onSaveClick: () -> Unit,
+                   modifier: Modifier) {
+
 }
