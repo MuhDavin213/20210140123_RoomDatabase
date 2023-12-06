cdb84980eb92bc53fd4cc1b2dd954b5c9deed988
MuhDavin213 <davin.new58@gmail.com>
2023-12-06 10:06:33 +0700

    Add function CreationExtrasaplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
+
+object PenyediaViewModel {
+
+}
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
