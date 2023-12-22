50fe182cd888785784c923db5ff609eb8b80c670
MuhDavin213 <davin.new58@gmail.com>
2023-12-22 13:11:22 +0700

    all Done

--- app/src/main/java/com/example/roomsiswa/data/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/data/DatabaseSiswa.kt
@@ -1,10 +1,11 @@
 package com.example.roomsiswa.data
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-
+@Database(entities = [Siswa::class], version = 2,exportSchema = false)
 abstract class DatabaseSiswa: RoomDatabase() {
     abstract fun siswaDao(): SiswaDao
 
